6e1736c4f2235dca7dcd270a99ce385bf80d9ece
Nicolas Renaud <nicolas.gm.renaud@gmail.com>
2024-04-24 13:07:17 +0200

    created repo architecture

---
+++ wntr_quantum/__init__.py
@@ -0,0 +1,8 @@
+"""Documentation about wntr_quantum."""
+import logging
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+__author__ = "Nicolas Renaud"
+__email__ = "n.renaud@esciencecenter.nl"
+__version__ = "0.1.0"
