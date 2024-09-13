63ae52ec7255f749038ae2ebb951dfd383a97d9e
Nicolas Renaud <nicolas.gm.renaud@gmail.com>
2024-09-13 18:08:37 +0200

    fix dw equation

--- wntr_quantum/sim/solvers/qubo_polynomial_solver.py
+++ wntr_quantum/sim/solvers/qubo_polynomial_solver.py
@@ -97,11 +97,15 @@ class QuboPolynomialSolver(object):
         """
         P0, P1, P2, P3 = self.matrices
         num_pipes = self.wn.num_pipes
-        p0 = P0.reshape(
-            -1,
-        )
-        p1 = P1[:, num_pipes:] + P2.sum(1)[:, num_pipes:]
-        p2 = P3.sum(1)[:, num_pipes:, num_pipes:].sum(-1)
+
+        if self.wn.options.hydraulic.headloss == "C-M":
+            p0 = P0.reshape(
+                -1,
+            )
+            p1 = P1[:, num_pipes:] + P2.sum(1)[:, num_pipes:]
+            p2 = P3.sum(1)[:, num_pipes:, num_pipes:].sum(-1)
+        elif self.wn.options.hydraulic.headloss == "D-W":
+            raise NotImplementedError("verufy_solution not implemented for DW")
         sign = np.sign(input)
         return p0 + p1 @ input + (p2 @ (sign * input * input))
 
@@ -120,13 +124,24 @@ class QuboPolynomialSolver(object):
         P0, P1, P2, P3 = self.matrices
         num_heads = self.wn.num_junctions
         num_pipes = self.wn.num_pipes
+        num_signs = self.wn.num_pipes
         num_vars = num_heads + num_pipes
 
-        p0 = P0.reshape(
-            -1,
-        )
-        p1 = P1[:, num_pipes:] + P2.sum(1)[:, num_pipes:]
-        p2 = P3.sum(1)[:, num_pipes:, num_pipes:].sum(-1)
+        if self.wn.options.hydraulic.headloss == "C-M":
+            p0 = P0.reshape(
+                -1,
+            )
+            p1 = P1[:, num_pipes:] + P2.sum(1)[:, num_pipes:]
+            p2 = P3.sum(1)[:, num_pipes:, num_pipes:].sum(-1)
+
+        elif self.wn.options.hydraulic.headloss == "D-W":
+            p0 = P0.reshape(
+                -1,
+            ) + P1[
+                :, :num_signs
+            ].sum(-1)
+            p1 = P1[:, num_pipes:] + P2.sum(1)[:, num_pipes:]
+            p2 = P3.sum(1)[:, num_pipes:, num_pipes:].sum(-1)
 
         def func(input):
             sign = np.sign(input)
